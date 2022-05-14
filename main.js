c7279b6b4bd529297e0f40bce27e43656241aec7
hayaba <Hayabara995@gmail.com>
2022-05-14 17:35:31 +0200

    arrays

---
+++ main.js
@@ -0,0 +1,40 @@
+let firstCard = 10;
+let secondCard = 11;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el"); //this is another method to grap an html element
+let cardsEl = document.getElementById("cards-el");
+
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+let messageEl = document.getElementById("message-el");
+
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + cards[0] + " - " + cards[1];
+    if (sum <= 20){
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    let card = 4;
+    sum += card;
+    cards.push(card);
+    renderGame();
+    console.log(cards)
+}
